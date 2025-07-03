32ecfdb87f7e21dd665e985c69b77eccfb409d52
Rian Weber <riankarlosweber13@gmail.com>
2025-07-03 19:57:49 +0000

    Commit com o Rest

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,3 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import dotenv from 'dotenv';
+
+// Constrói o caminho para o diretório raiz do projeto
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// O arquivo .env está dois níveis acima da pasta /api/src
+const projectRoot = path.resolve(__dirname, '..', '..'); 
+dotenv.config({ path: path.join(projectRoot, '.env') });
+
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import { transactionRoutes } from './routes'; 
